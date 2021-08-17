485a2771738e71546a4f073928f71532c3ba60d6
LigiaM <LigiaM@PALERMOL-LT>
2021-08-17 09:36:35 +0300

    Add project files.

---
+++ Online Donations/Areas/Identity/Data/Online_DonationsUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Online_Donations.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the Online_DonationsUser class
+    public class Online_DonationsUser : IdentityUser
+    {
+
+    [PersonalData]
+    public string Name { get; set; }
+
+    public bool IsAdmin { get; set; }
+   
+    }
+
+	public class AdditionalUserClaimsPrincipalFactory
+		: UserClaimsPrincipalFactory<Online_DonationsUser, IdentityRole>
+	{
+		public AdditionalUserClaimsPrincipalFactory(
+			UserManager<Online_DonationsUser> userManager,
+			RoleManager<IdentityRole> roleManager,
+			IOptions<IdentityOptions> optionsAccessor)
+			: base(userManager, roleManager, optionsAccessor)
+		{ }
+
+		public async override Task<ClaimsPrincipal> CreateAsync(Online_DonationsUser user)
+		{
+			var principal = await base.CreateAsync(user);
+			var identity = (ClaimsIdentity)principal.Identity;
+
+			var claims = new List<Claim>();
+			if (user.IsAdmin)
+			{
+				claims.Add(new Claim(ClaimTypes.Role, "admin"));
+			}
+			else
+			{
+				claims.Add(new Claim(ClaimTypes.Role, "user"));
+			}
+
+			identity.AddClaims(claims);
+			return principal;
+		}
+	}
+}
---
+++ Online Donations/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Online_Donations.Areas.Identity.Data;
+using Online_Donations.Models;
+
+[assembly: HostingStartup(typeof(Online_Donations.Areas.Identity.IdentityHostingStartup))]
+namespace Online_Donations.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Online_DonationsContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Online_DonationsContextConnection")));
+
+                services.AddDefaultIdentity<Online_DonationsUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddRoles<IdentityRole>()
+                    .AddEntityFrameworkStores<Online_DonationsContext>();
+                
+
+
+            });
+            
+        }
+    }
+}
